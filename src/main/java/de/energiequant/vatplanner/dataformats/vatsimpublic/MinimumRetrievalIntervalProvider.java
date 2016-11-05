6c514030713a770a98e8b917ac1fb236feb4345a
Daniel Neugebauer <dneuge@energiequant.de>
2016-11-05 12:40:35 +0100

    adding MinimumRetrievalIntervalProvider

---
+++ src/main/java/de/energiequant/vatplanner/dataformats/vatsimpublic/MinimumRetrievalIntervalProvider.java
@@ -0,0 +1,16 @@
+package de.energiequant.vatplanner.dataformats.vatsimpublic;
+
+import java.time.Duration;
+
+/**
+ * Should be implemented if network-retrieved data defines a minimum interval
+ * (minimum time to wait) before next fetch.
+ */
+public interface MinimumRetrievalIntervalProvider {
+    /**
+     * Returns the minimum interval (time to wait) before next fetch of data as
+     * requested by upstream sources.
+     * @return minimum interval at which data should be retrieved
+     */
+    public Duration getMinimumRetrievalInterval();
+}
