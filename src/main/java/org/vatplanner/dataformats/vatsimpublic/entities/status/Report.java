9996c39a8019ef67840797f176a4a378f4309845
Daniel Neugebauer <dneuge@energiequant.de>
2019-12-23 13:45:32 +0100

    Report: removed extra fields not used inside this library

--- src/main/java/org/vatplanner/dataformats/vatsimpublic/entities/status/Report.java
+++ src/main/java/org/vatplanner/dataformats/vatsimpublic/entities/status/Report.java
@@ -9,31 +9,11 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.vatplanner.dataformats.vatsimpublic.parser.DataFileMetaData;
-import org.vatplanner.dataformats.vatsimpublic.parser.NetworkInformation;
 
 /**
  * A report is equal to one parsed VATSIM status data file in imported form. Not
  * all information available from parsed data files is maintained as these
  * entities are intended to keep track of flights and facilities, not meta data.
- * Instead, some additional meta information related to fetching and
- * parsing/processing the retrieved information is added on import or can
- * optionally be set to keep track of information sources:
- * <ul>
- * <li>{@link #fetchTime} is the timestamp when data file was fetched (requested
- * from VATSIM servers)</li>
- * <li>{@link #fetchUrlRequested} holds the data file source URL requested from
- * VATSIM servers (as available from
- * {@link NetworkInformation#getDataFileUrls()} file)</li>
- * <li>{@link #fetchUrlRetrieved} holds the actually retrieved data file URL
- * after resolving all HTTP redirects</li>
- * <li>{@link #fetchNode} identifies the node who fetched the data from VATSIM
- * in a cluster setup</li>
- * <li>{@link #parseTime} is the timestamp when a previously fetched data file
- * was actually parsed</li>
- * <li>{@link #parserRejectedLines} is the number of lines of a data file that
- * could not be processed by the parser (any number >0 indicates loss of
- * information)</li>
- * </ul>
  */
 public class Report {
 
@@ -42,14 +22,6 @@ public class Report {
 
     private final Instant recordTime;
     private int numberOfConnectedClients;
-
-    private Instant fetchTime;
-    private String fetchUrlRequested;
-    private String fetchUrlRetrieved;
-    private String fetchNode;
-
-    private Instant parseTime;
-    private int parserRejectedLines = -1; // TODO: instead of lines count rejected clients; also count clients which were parsed but could not be imported
 
     /**
      * Creates a new report for the given timestamp.
@@ -158,121 +130,6 @@ public class Report {
 
     public Report setNumberOfConnectedClients(int numberOfConnectedClients) {
         this.numberOfConnectedClients = numberOfConnectedClients;
-        return this;
-    }
-
-    /**
-     * Returns the timestamp when the source data file was fetched (requested)
-     * from VATSIM servers.
-     *
-     * <p>
-     * This is an extra field to keep track of application-specific but
-     * generally useful meta-information.
-     * </p>
-     *
-     * @return timestamp of fetching the source data file
-     */
-    public Instant getFetchTime() {
-        return fetchTime;
-    }
-
-    public Report setFetchTime(Instant fetchTime) {
-        this.fetchTime = fetchTime;
-        return this;
-    }
-
-    /**
-     * Returns the URL originally requested to retrieve the source data file.
-     * This should usually be a URL listed in the {@link NetworkInformation}
-     * used at time of request.
-     *
-     * <p>
-     * This is an extra field to keep track of application-specific but
-     * generally useful meta-information.
-     * </p>
-     *
-     * @return URL data file was originally requested from (before redirects)
-     */
-    public String getFetchUrlRequested() {
-        return fetchUrlRequested;
-    }
-
-    public Report setFetchUrlRequested(String fetchUrlRequested) {
-        this.fetchUrlRequested = fetchUrlRequested;
-        return this;
-    }
-
-    /**
-     * Returns the URL the source data file was actually retrieved from after
-     * following all redirects. This may be a different URL than those listed in
-     * {@link NetworkInformation}.
-     *
-     * <p>
-     * This is an extra field to keep track of application-specific but
-     * generally useful meta-information.
-     * </p>
-     *
-     * @return URL data file was actually retrieved from (after redirects)
-     */
-    public String getFetchUrlRetrieved() {
-        return fetchUrlRetrieved;
-    }
-
-    public Report setFetchUrlRetrieved(String fetchUrlRetrieved) {
-        this.fetchUrlRetrieved = fetchUrlRetrieved;
-        return this;
-    }
-
-    /**
-     * Returns the identification of the cluster node who fetched the data file.
-     *
-     * <p>
-     * This is an extra field to keep track of application-specific but
-     * generally useful meta-information.
-     * </p>
-     *
-     * @return ID of cluster node who fetched the data file
-     */
-    public String getFetchNode() {
-        return fetchNode;
-    }
-
-    public Report setFetchNode(String fetchNode) {
-        this.fetchNode = fetchNode;
-        return this;
-    }
-
-    /**
-     * Returns the timestamp when a data file was parsed/processed.
-     *
-     * <p>
-     * This is an extra field to keep track of application-specific but
-     * generally useful meta-information.
-     * </p>
-     *
-     * @return timestamp of parsing/processing data file
-     */
-    public Instant getParseTime() {
-        return parseTime;
-    }
-
-    public Report setParseTime(Instant parseTime) {
-        this.parseTime = parseTime;
-        return this;
-    }
-
-    /**
-     * Returns the number of lines rejected by the parser. A number >0 indicated
-     * loss of information. If not set, a negative value will be returned.
-     *
-     * @return number of lines rejected by parser; negative if not set
-     */
-    public int getParserRejectedLines() {
-        return parserRejectedLines;
-    }
-
-    public Report setParserRejectedLines(int parserRejectedLines) {
-        this.parserRejectedLines = parserRejectedLines;
         return this;
     }
 
