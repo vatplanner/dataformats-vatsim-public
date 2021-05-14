4a426593301ef3daa4d0c90c50e0b15990db5262
Daniel Neugebauer <dneuge@energiequant.de>
2021-05-14 19:42:58 +0200

    GeoPoint2D: equals and hashCode

--- src/main/java/org/vatplanner/dataformats/vatsimpublic/utils/GeoPoint2D.java
+++ src/main/java/org/vatplanner/dataformats/vatsimpublic/utils/GeoPoint2D.java
@@ -1,10 +1,19 @@
 package org.vatplanner.dataformats.vatsimpublic.utils;
 
+import java.util.Objects;
+
+/**
+ * Represents a 2-dimensional geographic point/coordinate.
+ */
 public class GeoPoint2D {
     private final double latitude;
     private final double longitude;
 
+    // TODO: add static method to create normalized point by modulo
+
     public GeoPoint2D(double latitude, double longitude) {
+        // TODO: reject out of range values
+        // TODO: unify full longitude deflection? (-180 equals 180 for same latitude)
         this.latitude = latitude;
         this.longitude = longitude;
     }
@@ -20,5 +29,25 @@ public class GeoPoint2D {
     @Override
     public String toString() {
         return String.format("GeoPoint2D[lat=%f, lon=%f]", latitude, longitude);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(latitude, longitude);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof GeoPoint2D)) {
+            return false;
+        }
+
+        GeoPoint2D other = (GeoPoint2D) obj;
+
+        if ((other.latitude == this.latitude) && (other.longitude == this.longitude)) {
+            return true;
+        }
+
+        return false;
     }
 }
---
+++ src/test/java/org/vatplanner/dataformats/vatsimpublic/utils/GeoPoint2DTest.java
@@ -0,0 +1,117 @@
+package org.vatplanner.dataformats.vatsimpublic.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class GeoPoint2DTest {
+    @Test
+    public void testEquals_null_returnsFalse() {
+        // Arrange
+        GeoPoint2D point = new GeoPoint2D(0.0, 0.0);
+
+        // Act
+        boolean result = point.equals(null);
+
+        // Assert
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testEquals_differentClass_returnsFalse() {
+        // Arrange
+        GeoPoint2D point = new GeoPoint2D(0.0, 0.0);
+
+        // Act
+        boolean result = point.equals(new Object());
+
+        // Assert
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testEquals_sameInstance_returnsTrue() {
+        // Arrange
+        GeoPoint2D point = new GeoPoint2D(12.3, 123.4);
+
+        // Act
+        boolean result = point.equals(point);
+
+        // Assert
+        assertThat(result).isTrue();
+    }
+
+    public static Stream<Arguments> dataProviderEqualCoordinates() {
+        return Stream.of(
+            Arguments.of(new GeoPoint2D(-90.0, -180.0), new GeoPoint2D(-90.0, -180.0)),
+            Arguments.of(new GeoPoint2D(90.0, 180.0), new GeoPoint2D(90.0, 180.0)),
+            Arguments.of(new GeoPoint2D(0.1, 0.1), new GeoPoint2D(0.1, 0.1)) //
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProviderEqualCoordinates")
+    public void testEquals_equalCoordinates_returnsTrue(GeoPoint2D pointA, GeoPoint2D pointB) {
+        // Arrange (nothing to do)
+
+        // Act
+        boolean result = pointA.equals(pointB);
+
+        // Assert
+        assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProviderEqualCoordinates")
+    public void testHashCode_equalCoordinates_equalHashCode(GeoPoint2D pointA, GeoPoint2D pointB) {
+        // Arrange (nothing to do)
+
+        // Act (nothing to do)
+
+        // Assert
+        assertThat(pointA).hasSameHashCodeAs(pointB);
+    }
+
+    public static Stream<Arguments> dataProviderDifferentCoordinates() {
+        return Stream.of(
+            // full deflection
+            Arguments.of(new GeoPoint2D(-90.0, -180.0), new GeoPoint2D(90.0, 180.0)),
+
+            // moving
+            Arguments.of(new GeoPoint2D(0.1, 0.0), new GeoPoint2D(0.0, 0.0)),
+            Arguments.of(new GeoPoint2D(0.0, 0.1), new GeoPoint2D(0.0, 0.0)),
+
+            // crossed
+            Arguments.of(new GeoPoint2D(0.1, 0.0), new GeoPoint2D(0.0, 0.1)) //
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProviderDifferentCoordinates")
+    public void testEquals_differentCoordinatesCheckingAEqualsB_returnsFalse(GeoPoint2D pointA, GeoPoint2D pointB) {
+        // Arrange (nothing to do)
+
+        // Act
+        boolean result = pointA.equals(pointB);
+
+        // Assert
+        assertThat(result).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProviderDifferentCoordinates")
+    public void testEquals_differentCoordinatesCheckingBEqualsA_returnsFalse(GeoPoint2D pointA, GeoPoint2D pointB) {
+        // Arrange (nothing to do)
+
+        // Act
+        boolean result = pointB.equals(pointA);
+
+        // Assert
+        assertThat(result).isFalse();
+    }
+}
